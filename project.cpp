eb5ab87620f8c35a8461c97a9a7ef7de74983df6
mario_santos <75098183+mario-santosgithub@users.noreply.github.com>
2022-01-07 18:11:54 +0000

    update project.cpp

--- project.cpp
+++ project.cpp
@@ -4,153 +4,137 @@
 #include <string>
 #include <unordered_map>
 
-
-// Prototypes
+// Prototypes for functions
 void problemOne();
 void getSolutionsP1(int number);
 void problemTwo();
 void getSolutionsP2(int number);
 
-
 // Global variables
-std::unordered_map<int, int> umap;
 std::vector<int> firstInput, aux, ac, al;
-int maxLength = 0, counter=0, size1 = 1, size2 = 0;
+int maxLength = 1, counter = 1, size1 = 1, size2 = 0;
 
 // Main function
 int main() {
     int problem;
-    std::cin >> problem; // reads the problem (first input line)
+    std::cin >> problem; // receives the problem (first input line)
 
-    if (problem == 1) {
-        problemOne();
-    }
-    else if (problem == 2) {
-        problemTwo();
-    }
+    if (problem == 1) { problemOne(); }
+    else if (problem == 2) { problemTwo(); }
+
     return 0;
 }
 
-// Funtion to solve problem 1
+// Function to solve problem one
 void problemOne() {
-
-    int f;
+    int number;
     std::string str, temp;
     std::stringstream ss;
 
-    std::cin >> f;
-    firstInput.push_back(f);
-    al.push_back(1);
-    ac.push_back(1);
 
-    std::getline(std::cin, str);
+    std::cin >> number;
+    firstInput.push_back(number);
+    al.push_back(1); // the maxlength is currently 1
+    ac.push_back(1); // the counter is currently 1
+
+    std::getline(std::cin, str); // gets the line, without the first number
     ss << str;
 
-    maxLength = 1;
-    counter = 1;
+    // if only 1 number
+    if (str == " ") { std::cout << maxLength << " " << counter << std::endl; return; }
 
-    if (str == "") { std::cout << maxLength << " " << counter << std::endl; return; }
-    
-    while (!ss.eof()) {
-  
+    while (!ss.eof()) { // while it doesnt reach the end of the line
         ss >> temp;
-  
-        if (std::stringstream(temp) >> f) {
-            al.push_back(1);
-            ac.push_back(1);
+        
+        if (std::stringstream(temp) >> number) { // if its a number
+            getSolutionsP1(number);
             size1++;
-            getSolutionsP1(f);
-            firstInput.push_back(f);
+            firstInput.push_back(number);
         }
-        temp = "";
+        temp = ""; // reset the string
     }
 
     std::cout << maxLength << " " << counter << std::endl;
-    return;    
+    return;
 }
 
-
+// Function to get the solutions for problem one
 void getSolutionsP1(int number) {
-    int len=1, count=1;
-    for (int i=0; i < size1-1; i++) {
+    int len = 1, count = 1;
+    for (int i=0; i < size1; i++) { // check the previous numbers
 
-        if (number > firstInput[i]) {
-            if (al[i] + 1 > len) {
+        if (number > firstInput[i]) { // if number is bigger than a previous
+            if (al[i]+1 > len) { // and the lenght+1 will be bigger
 
                 len = al[i] + 1;
                 count = ac[i];
-                if (len > maxLength) { 
+                if (len > maxLength) {
                     maxLength = len;
                     counter = 0;
                 }
             }
-            else if (al[i] + 1 == len) {
-                count += ac[i];
+            else if (al[i] + 1 == len) { // if the same length is reached, add
+                count += ac[i];     //  one to the count
             }
         }
     }
-    al[size1-1] = len;
-    ac[size1-1] = count;
+    al.push_back(len); // save the two values in the vectors
+    ac.push_back(count);
 
     if (len == maxLength) { counter += count; }
 }
 
+// Function to solve problem two
 void problemTwo() {
 
-    int f;
+    int number;
     std::string str, temp;
     std::stringstream ss, ss1;
-    std::cin >> f;
-    firstInput.push_back(f);
-    umap[f] = f;
-    aux.push_back(0);
+    std::unordered_map<int, int> umap; //used to save the first input values
+
+    std::cin >> number;
+    firstInput.push_back(number);
+    umap[number] = number;
+    aux.push_back(0); // the aux vector starts with the value at 0
 
     std::getline(std::cin, str);
     ss << str;
-    
-    // receives the first input and stores it in a vector
+
+    // receives the first input and stores it
     while (!ss.eof()) {
         ss >> temp;
-        if (std::stringstream(temp) >> f) {
-            firstInput.push_back(f);
-            umap[f] = f;
+        if (std::stringstream(temp) >> number) {
+            firstInput.push_back(number);
+            umap[number] = number;
             size1++;
-            aux.push_back(0);
+            aux.push_back(0); // the aux vector starts the value at 0
         }
         temp = "";
     }
 
-    
     // second input
-    std::cin >> f;
-    getSolutionsP2(f);
+    std::cin >> number;
+    getSolutionsP2(number);
     size2++;
 
     std::getline(std::cin, str);
     ss1 << str;
 
-
-    while(!ss1.eof()) {
+    while (!ss1.eof()) {
         ss1 >> temp;
-        if (std::stringstream(temp) >> f) {
-            if(umap.find(f) != umap.end()) {
-                getSolutionsP2(f);
+        if (std::stringstream(temp) >> number) {
+            if (umap.find(number) != umap.end()) {
+                getSolutionsP2(number);
                 size2++;
             }
         }
         temp = "";
     }
-    for (int i=0; i<size1; i++) {
-		if (aux[i] > maxLength) {
-            maxLength = aux[i];
-        }
-    }
 
     std::cout << maxLength << std::endl;
-
 }
 
-
+// Funtion to get solution to problem two
 void getSolutionsP2(int number) {
     
     int length = 0;
@@ -158,6 +142,7 @@ void getSolutionsP2(int number) {
         
         if (number == firstInput[i] && length+1 > aux[i]) {
             aux[i] = length + 1;
+            if (length+1 > maxLength) {maxLength = length+1;}
             
         }
         if (number > firstInput[i] && aux[i] > length) {
